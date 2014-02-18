294bc0161c38e191ec824b14e2ee3865f8a728cd
Billy Onjea <vasili.onjea@gmail.com>
2014-02-18 21:08:53 +0100

    Close GH-10: General improvements to CommentsParser.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,27 @@
 ;(function(_, window, undefined) {
   'use strict';
 
-  var commentParser = new app.CommentsParser({
+  var output = document.getElementById('shared-links');
+
+  new app.CommentsParser({
     url: 'https://api.github.com/repos/tastejs/TasteMusic/issues/1/comments',
-    container: document.getElementById('shared-links')
-  });
+    done: parseDone,
+    fail: parseFail
+  }).parse();
 
-  commentParser.init();
 
+  // The sharedLinks are available here.
+  function parseDone(result) {
+    if (result && _.isObject(result)) {
+      output.innerHTML = result.htmlList.outerHTML;
+
+      console.log(result);
+    }
+  }
+
+  function parseFail(errMessage) {
+    if (errMessage) {
+      output.innerHTML = errMessage;
+    }
+  }
 }(_, window));
